b836e1bcb2884804857144f469f838528a141e96
Dominic Aguilar <dmo.dbla@gmail.com>
2025-03-28 00:57:13 -0400

    added 2 items, 2 projs

--- Content/Items/FunnyFish.cs
+++ Content/Items/FunnyFish.cs
@@ -1,30 +1,31 @@
+// MISSING an accessory sprite
 
 namespace TheBindingOfRarria.Content.Items
 {
-    public class FishTank : ModItem
+    public class FunnyFish : ModItem
     {
+
         public override void SetDefaults()
         {
             Item.accessory = true;
-            Item.height = 36;
-            Item.width = 34;
+            Item.width = 30;
+            Item.height = 30;
             Item.rare = ItemRarityID.Green;
+
         }
 
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
-            player.GetModPlayer<FishTankPlayer>().IsTuna = true;
+            player.GetModPlayer<FunnyPlayer>().IsFunny = true;
         }
     }
 
-    public class FishTankPlayer : ModPlayer
+    public class FunnyPlayer : ModPlayer
     {
-        public bool IsTuna;
+        public bool IsFunny;
         public override void ResetEffects()
         {
-            if (Player.timeSinceLastDashStarted <= 1 && Main.myPlayer == Player.whoAmI && IsTuna)
-                Projectile.NewProjectile(Player.GetSource_FromThis(), new Vector2(Player.Center.X + (Player.direction * Player.velocity.X), Player.Center.Y), new Vector2(Player.velocity.X, Player.velocity.Y - 2f), ModContent.ProjectileType<FlippityFloppity>(), 200, 2, Main.myPlayer);
-            IsTuna = false;
+            IsFunny = false;
         }
 
         public override void CatchFish(FishingAttempt attempt, ref int itemDrop, ref int npcSpawn, ref AdvancedPopupRequest sonar, ref Vector2 sonarPosition)
@@ -33,9 +34,20 @@ namespace TheBindingOfRarria.Content.Items
             // Only made the ocean require 1k water instead of location for Skyblock players
             if (Main.rand.NextBool(25) && inWater && attempt.heightLevel == 1 && attempt.waterTilesCount >= 1000 && !attempt.veryrare && !attempt.legendary && attempt.rare)
             {
-                itemDrop = ModContent.ItemType<FishTank>();
+                itemDrop = ModContent.ItemType<FunnyFish>();
                 return;
             }
         }
+        public override void ModifyShootStats(Item item, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            if (IsFunny && type == ProjectileID.Bullet)
+            {
+                type = ModContent.ProjectileType<FishBullet>();
+                damage += 12;
+            }
+        }
     }
+
 }
+
+
