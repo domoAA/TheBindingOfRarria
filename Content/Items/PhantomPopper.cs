3be98f20637ed779b2f93c062ce405b120881def
KainSett <7talessett@gmail.com>
2025-04-21 14:18:48 +0300

    Rewrite of the latest pulls

--- Content/Items/PhantomPopper.cs
+++ Content/Items/PhantomPopper.cs
@@ -1,25 +1,42 @@
-using Microsoft.Xna.Framework.Graphics;
+using System.IO;
 using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
 
 namespace TheBindingOfRarria.Content.Items;
 
 public class PhantomPopper : ModItem
 {
     public override string Texture => ContentPath + "Items/" + Name;
-
     public override void SetDefaults()
     {
-        Item.DefaultToAccessory(22, 26);
+        Item.accessory = true;
+        Item.width = 22;
+        Item.height = 26;
+        Item.rare = ItemRarityID.Yellow;
+        Item.value = Item.buyPrice(0, 4, 80, 42);
     }
 
     public override void UpdateAccessory(Player player, bool hideVisual)
     {
-        // Устанавливаем флаг, чтобы GlobalProjectile знал, что аксессуар экипирован
         player.GetModPlayer<PhantomPopperPlayer>().HasPhantomPopper = true;
     }
 }
+
+public class PhantomCasterDrop : GlobalNPC
+{
+    public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+    {
+        if (npc.type == NPCID.RaggedCaster || npc.type == NPCID.RaggedCasterOpenCoat)
+            npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<PhantomPopper>(), 17));
+
+        base.ModifyNPCLoot(npc, npcLoot);
+    }
+}
+
 public class PhantomPopperPlayer : ModPlayer
 {
     public bool HasPhantomPopper = false;
@@ -32,50 +49,85 @@ public class PhantomPopperPlayer : ModPlayer
 
 public class PhantomPopperGlobalProjectile : GlobalProjectile
 {
-    private bool initialized = false;
-    private float splitTimer = 180;
-    private bool noSplit = false;
+    public int counter = -1;
+    public bool ASoul = false;
 
     public override bool InstancePerEntity => true;
 
-    public override void AI(Projectile projectile)
+    public override bool AppliesToEntity(Projectile entity, bool lateInstantiation)
     {
-        if (projectile.DamageType == DamageClass.Magic && projectile.owner == Main.myPlayer && !noSplit)
+        return entity.damage > 0 && entity.DamageType == DamageClass.Magic && entity.friendly;
+    }
+
+    public override void OnSpawn(Projectile projectile, IEntitySource source)
+    {
+        if (source != null && source.Context != null && source.Context == "Phantom Popper accessory")
         {
-            Player player = Main.player[projectile.owner];
+            projectile.penetrate = 1;
+            projectile.timeLeft = 120;
+            projectile.scale *= 0.5f;
+            ASoul = true;
+        }
+        else if (Main.player[projectile.owner].GetModPlayer<PhantomPopperPlayer>().HasPhantomPopper)
+        {
+            counter = 180;
+        }
+    }
 
-            if (player.GetModPlayer<PhantomPopperPlayer>().HasPhantomPopper && !initialized)
+    public override void SendExtraAI(Projectile projectile, BitWriter bitWriter, BinaryWriter binaryWriter)
+    {
+        bitWriter.WriteBit(ASoul);
+
+        if (ASoul)
+        {
+            binaryWriter.Write(projectile.penetrate);
+            binaryWriter.Write(projectile.timeLeft);
+            binaryWriter.Write(projectile.scale);
+        }
+    }
+
+    public override void ReceiveExtraAI(Projectile projectile, BitReader bitReader, BinaryReader binaryReader)
+    {
+        var flag = bitReader.ReadBit();
+
+        if (!ASoul)
+            ASoul = flag;
+
+        if (ASoul)
+        {
+            projectile.penetrate = binaryReader.ReadInt32();
+            projectile.timeLeft = binaryReader.ReadInt32();
+            projectile.scale = binaryReader.ReadSingle();
+        }
+    }
+
+    public override void PostAI(Projectile projectile)
+    {
+        if (projectile.owner == Main.myPlayer && !ASoul && counter != -1)
+        {
+            counter--;
+
+            Player owner = Main.player[projectile.owner];
+
+            if (owner.GetModPlayer<PhantomPopperPlayer>().HasPhantomPopper)
+
+
+            if (counter <= 10)
             {
-                initialized = true;
-            }
-
-            if (initialized)
-            {
-                splitTimer--;
-
-                if (splitTimer <= 0)
+                for (int i = 0; i < 3; i++)
                 {
-                    for (int i = 0; i < 3; i++)
-                    {
-                        Vector2 velocity = new Vector2(Main.rand.NextFloat(-2f, 2f), Main.rand.NextFloat(-2f, 2f));
-                        int proj = Projectile.NewProjectile(
-                            player.GetSource_FromThis(),
-                            projectile.Center,
-                            velocity,
-                            ProjectileID.LostSoulFriendly,
-                            projectile.damage / 3,
-                            projectile.knockBack,
-                            player.whoAmI
-                        );
-
-                        Main.projectile[proj].GetGlobalProjectile<PhantomPopperGlobalProjectile>().noSplit = true;
-                        Main.projectile[proj].timeLeft = 300;
-                        Main.projectile[proj].tileCollide = false;
-                    }
-
-                    // Уничтожаем оригинальный снаряд
-                    projectile.Kill();
+                    Vector2 velocity = new Vector2(2).RotatedByRandom(TwoPi);
+                    int proj = Projectile.NewProjectile(
+                        owner.GetSource_FromThis("Phantom Popper accessory"),
+                        projectile.Center,
+                        velocity,
+                        ProjectileID.LostSoulFriendly,
+                        projectile.damage / 3,
+                        projectile.knockBack,
+                        owner.whoAmI
+                    );
                 }
+                projectile.Kill();
             }
         }
     }
--- Content/Projectiles/SlowingAura.cs
+++ Content/Projectiles/SlowingAura.cs
@@ -14,8 +14,6 @@ public class SlowingAura : ModProjectile
 
     public override void SetDefaults()
     {
-        Main.spawnTileX = Main.LocalPlayer.Center.ToTileCoordinates().X;
-        Main.spawnTileY = Main.LocalPlayer.Center.ToTileCoordinates().Y;
         Projectile.tileCollide = false;
         Projectile.penetrate = -1;
         Projectile.ignoreWater = true;
